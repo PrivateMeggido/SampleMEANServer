53ad956e66ed712e5f8fcf28d8ef929c69dc304d
Juan M. Medina <Juan M. Medina>
2016-02-23 09:14:58 -0600

    Initial commit

---
+++ built/local/src/models/developerModel.js
@@ -0,0 +1,5 @@
+var mongoose = require('mongoose');
+exports.developerSchema = new mongoose.Schema({
+    name: String
+});
+exports.repository = mongoose.model('Developer', exports.developerSchema);
---
+++ built/local/src/routes/developerRoutes.js
@@ -0,0 +1,11 @@
+var controller = require('../controller/developerController');
+function getAll(req, res) {
+    controller.getDevelopers(req, res);
+}
+exports.getAll = getAll;
+;
+function create(req, res) {
+    controller.createDeveloper(req, res);
+}
+exports.create = create;
+;
